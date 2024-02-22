e52229b378578f91b3649481aca8db3c4399a21f
Carlos <carlosfremdling@gmail.com>
2024-02-22 19:59:17 -0300

    fix: ajuste da mensagem do IMC

--- main.js
+++ main.js
@@ -16,29 +16,66 @@ button.addEventListener("click", () => {
   if (resultado < 18.5) {
     span.innerHTML = `Seu IMC é ${resultado.toFixed(2)} - Abaixo do Peso`;
     span.classList.add('danger');
+    if (span.classList.contains('success')) {
+      span.classList.remove('success');
+    }
+    if (span.classList.contains('warning')) {
+      span.classList.remove('warning');
+    }
+
   } else {
     if (resultado >= 18.5 && resultado <= 24.9) {
       span.innerHTML = `Seu IMC é ${resultado.toFixed(2)}  - Peso Normal`;
       span.classList.add('success');
+      if (span.classList.contains('danger')) {
+        span.classList.remove('danger');
+      }
+      if (span.classList.contains('warning')) {
+        span.classList.remove('warning');
+      }
     } else {
       if (resultado >= 25 && resultado <= 29.9) {
         span.innerHTML = `Seu IMC é ${resultado.toFixed(2)}   - Sobrepeso`;
         span.classList.add('warning');
+        if (span.classList.contains('success')) {
+          span.classList.remove('success');
+        }
+        if (span.classList.contains('danger')) {
+          span.classList.remove('danger');
+        }
       } else {
         if (resultado >= 30 && resultado <= 34.9) {
           span.innerHTML =
             `Seu IMC é ${resultado.toFixed(2)} - Obesidade Grau I`;
             span.classList.add('danger');
+            if (span.classList.contains('success')) {
+              span.classList.remove('success');
+            }
+            if (span.classList.contains('warning')) {
+              span.classList.remove('warning');
+            }
         } else {
           if (resultado >= 35 && resultado <= 39.9) {
             span.innerHTML =
               `Seu IMC é ${resultado.toFixed(2)} - Obesidade Grau II`;
               span.classList.add('danger');
+              if (span.classList.contains('success')) {
+                span.classList.remove('success');
+              }
+              if (span.classList.contains('warning')) {
+                span.classList.remove('warning');
+              }
           } else {
             if (resultado >= 40) {
               span.innerHTML =
                 `Seu IMC é ${resultado.toFixed(2)} - Obesidade Grau III`;
                 span.classList.add('danger');
+                if (span.classList.contains('success')) {
+                  span.classList.remove('success');
+                }
+                if (span.classList.contains('warning')) {
+                  span.classList.remove('warning');
+                }
             }
           }
         }
